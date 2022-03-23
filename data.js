c6f3662208f284995d75c5cc7f5ae02558697ad7
Chandrakant1 G <Chandrakant1.G@ril.com>
2022-03-23 17:26:55 +0530

    Making simple api commit

---
+++ data.js
@@ -0,0 +1,7 @@
+data=[{name:"Chandrakant2", email:"chandrakant@gmail.com"},
+{name:"Chandrakant3", email:"chandrakant3@gmail.com"},
+{name:"Chandrakant4", email:"chandrakant5@gmail.com"}
+]
+
+
+module.exports=data;
--- index.js
+++ index.js
@@ -5,6 +5,14 @@ http.createServer(function(req,res)
     res.write(JSON.stringify({name:"tt"}));//Write date using response objevt
     res.end();//Tell server we are end
 }).listen(8000);//Listen to this port(we have ip adderess(locahost in this case): 8000 is port)*/
-var colors = require('colors');
+//var colors = require('colors');
  
-console.log('hello'.green);
+//console.log('hello'.blue);
+http=require('http');//import http module(inbuilt module)
+data=require('./data')
+http.createServer(function(req,res)
+{
+    res.writeHead(200,{'Content-type':'application/json'});
+    res.write(JSON.stringify(data));//Write date using response objevt
+    res.end();//Tell server we are end
+}).listen(8000);//Listen to this port(we have ip adderess(locahost in this case): 8000 is port)*/
