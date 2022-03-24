ccfd0287092502f41534f3207c51211b3c3317c7
Chandrakant1 G <Chandrakant1.G@ril.com>
2022-03-24 10:33:13 +0530

    file input added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ http.createServer(function(req,res)
 //var colors = require('colors');
  
 //console.log('hello'.blue);
+/*
 http=require('http');//import http module(inbuilt module)
 data=require('./data')
 http.createServer(function(req,res)
@@ -16,3 +17,21 @@ http.createServer(function(req,res)
     res.write(JSON.stringify(data));//Write date using response objevt
     res.end();//Tell server we are end
 }).listen(8000);//Listen to this port(we have ip adderess(locahost in this case): 8000 is port)*/
+
+//Add command input
+
+fs=require('fs');
+
+const input=process.argv;
+
+if(input[2]=='add')
+{
+    fs.writeFileSync(input[3],input[4]);
+}else if(input[2]=='remove')
+{
+    fs.unlinkSync(input[3]);
+}
+else
+{
+    console.log('invalid input')
+}
